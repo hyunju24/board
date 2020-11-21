09c105139c57f2b304ed9d5d0a3e84730834d4d9
hyunju24 <tlaguswn2249@gmail.com>
2020-11-21 22:35:30 +0900

    sendresult

--- models/List.js
+++ models/List.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose');
 // schema
 var listSchema = mongoose.Schema({
   name: {type:String, required:[true]},
+  img: {type:String, required:[true]},
   face_rate:{type:Number, required:[true]},
   pose_rate:{type:Number, required:[true]},
   fc_angry:{type:Number, required:[true]},
@@ -13,7 +14,8 @@ var listSchema = mongoose.Schema({
   fc_neutral:{type:Number, required:[true]},
   fc_sad:{type:Number, required:[true]},
   fc_smile:{type:Number, required:[true]},
-  fc_surprise:{type:Number, required:[true]}
+  fc_surprise:{type:Number, required:[true]},
+  result:{type:Array},
   //category: {type:String, required:[true]},
   //참조 구현 
   //author:{type:mongoose.Schema.Types.ObjectId, ref:'user', required:true},
--- routes/lists.js
+++ routes/lists.js
@@ -7,6 +7,7 @@ var List = require('../models/List');
 router.get('/', async function(req, res){
     await List.find({})
     .populate('_id')
+    .sort('_id')
     .exec(function(err, lists){
       if(err) return res.json(err);
       res.render('lists/index', {lists:lists});
@@ -14,12 +15,37 @@ router.get('/', async function(req, res){
   });
 
 
-router.get('/image1', async function(req, res){
-  await List.findOne({name:'image1'})
+router.get('/:name', async function(req, res){
+  await List.findOne({name:req.params.name})
     .exec(function(err, lists){
       if(err) return res.json(err);
       res.render('lists/test', {lists:lists});
     });
+
+
+// update
+router.put('/:name', function(req, res, next){
+  List.findOne({name:req.params.name})
+    .exec(function(err, list){
+      if(err) return res.json(err);
+
+      // update user object
+      //user.originalPassword = user.password;
+      //user.password = req.body.newPassword? req.body.newPassword : user.password;
+      //for(var p in req.body){
+      //  user[p] = req.body[p];
+      //}
+      list.result.push(req.body.userresults);
+
+      // save updated user
+      list.save(function(err, list){
+        if(err){
+          req.flash('errors', parseError(err));
+        }
+        res.redirect('/lists/'+list.name);
+      });
+  });
+});
   //console.log(zzal);
  // res.render('lists/test', {zzal : zzal});
   /* fs.readFile('views/lists/test.html', function(error, data){
