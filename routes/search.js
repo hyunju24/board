eea1232050480cc3be448abad2e86376a7a348e5
hyunju24 <tlaguswn2249@gmail.com>
2020-12-01 01:50:05 +0900

    search 수정

--- routes/search.js
+++ routes/search.js
@@ -1,7 +1,8 @@
 var express  = require('express');
 var router = express.Router();
+var multer = require('multer'); // 1
+var upload = multer({ dest: 'public/uploadedFiles/' });
 var Post = require('../models/Post');
-var User = require('../models/User');
 var Comment = require('../models/Comment');
 var File = require('../models/File'); // 3
 var util = require('../util');
@@ -44,6 +45,7 @@ router.get('/', async function(req, res){
         });
 });
 
+//show
 router.get('/:id', function(req, res){
   var commentForm = req.flash('commentForm')[0] || {_id: null, form: {}};
   var commentError = req.flash('commentError')[0] || { _id:null, parentComment: null, errors:{}};
@@ -61,6 +63,50 @@ router.get('/:id', function(req, res){
       console.log('err: ', err);
       return res.json(err);
     });
+});
+
+// edit
+router.get('/:id/edit', util.isLoggedin, checkPermission, function(req, res){
+  var post = req.flash('post')[0];
+  var errors = req.flash('errors')[0] || {};
+  if(!post){
+    Post.findOne({_id:req.params.id})                           // 1
+      .populate({path:'attachment',match:{isDeleted:false}})    // 1
+      .exec(function(err, post){                                // 1
+        if(err) return res.json(err);
+        res.render('search/edit', { post:post, errors:errors });
+      });
+  }
+  else {
+    post._id = req.params.id;
+    res.render('search/edit', { post:post, errors:errors });
+  }
+});
+
+// update
+router.put('/:id', util.isLoggedin, checkPermission, upload.single('newAttachment'), async function(req, res){
+  var post = await Post.findOne({_id:req.params.id}).populate({path:'attachment',match:{isDeleted:false}});
+  if(post.attachment && (req.file || !req.body.attachment)){
+    post.attachment.processDelete();
+  }
+  req.body.attachment = req.file?await File.createNewInstance(req.file, req.user._id, req.params.id):post.attachment;
+  req.body.updatedAt = Date.now();
+  Post.findOneAndUpdate({_id:req.params.id}, req.body, {runValidators:true}, function(err, post){
+    if(err){
+      req.flash('post', req.body);
+      req.flash('errors', util.parseError(err));
+      return res.redirect('/search/'+req.params.id+'/edit'+res.locals.getPostQueryString());
+    }
+    res.redirect('/search/'+req.params.id+res.locals.getPostQueryString());
+  });
+});
+
+// destroy
+router.delete('/:id', util.isLoggedin, checkPermission, function(req, res){
+  Post.deleteOne({_id:req.params.id}, function(err){
+    if(err) return res.json(err);
+    res.redirect('/search'+res.locals.getPostQueryString());
+  });
 });
 
 async function createSearchQuery(queries){ // 4
@@ -93,3 +139,13 @@ async function createSearchQuery(queries){ // 4
 }
 
 module.exports = router;
+
+// private functions
+function checkPermission(req, res, next){
+  Post.findOne({_id:req.params.id}, function(err, post){
+    if(err) return res.json(err);
+    if(post.author != req.user.id) return util.noPermission(req, res);
+
+    next();
+  });
+}
